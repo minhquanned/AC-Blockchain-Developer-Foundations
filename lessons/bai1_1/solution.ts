43d8273bc3f39fea9c843da8aa077fae1d8e78dc
Minh Quan <minhquan4so9@gmail.com>
2026-03-12 22:29:01 +0700

    finished bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import * as crypto from 'crypto';
 
 export type Block = {
   index: number;
@@ -9,5 +10,7 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const dataToHash = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const calculatedHash = crypto.createHash('sha256').update(dataToHash).digest('hex');
+  return calculatedHash === block.current_hash;
 }
